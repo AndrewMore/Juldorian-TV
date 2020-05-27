d610029fb2ea958b11fc87bb2f7201299508d7ca
Andrew More <recondomore@gmail.com>
2020-05-27 15:08:09 -0500

    Pre-merge commit
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -4,8 +4,8 @@ var VideoPlayer = (props) => (
       <iframe className="embed-responsive-item" src={'https://www.youtube.com/embed/' + props.video.id.videoId + '?&autoplay=1'}></iframe>
     </div>
     <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
+      <h3>{props.video.snippet.title}</h3>
+      <div>{props.video.snippet.description}</div>
     </div>
   </div>
 );
